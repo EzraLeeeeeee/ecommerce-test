d3cf5855a27e6aae596cab3958a29b1452d1d75b
Ezra <blackman002611@gmail.com>
2025-02-18 16:26:48 +0800

    加入 GitHub Actions 自動測試1

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,9 +41,6 @@ def driver(request):
 
     if browser == "chrome":
         options = ChromeOptions()
-        options.add_argument("--headless")  # 无界面模式
-        options.add_argument("--no-sandbox")  # 适用于无 root 权限环境
-        options.add_argument("--disable-dev-shm-usage")  # 解决共享内存问题
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
 
