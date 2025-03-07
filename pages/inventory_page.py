84a65310cc6ebc64057c3909460aa761d067cf26
Ezra <blackman002611@gmail.com>
2025-03-07 08:40:59 +0800

    加入GitHub Actions 自動測試與Allure測試報告3/7

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class InventoryPage:
     def __init__(self, driver):
@@ -24,3 +26,24 @@ class InventoryPage:
         """ 點擊購物車按鈕 """
         self.driver.find_element(*self.cart_icon).click()
 
+    def add_multiple_items_to_cart(self, item_names):
+        """
+        加入多個指定的商品到購物車
+        :param item_names: 商品名稱的列表，例如 ["Sauce Labs Backpack", "Sauce Labs Bike Light"]
+        """
+        for item_name in item_names:
+            item_xpath = f"//div[text()='{item_name}']/ancestor::div[@class='inventory_item']//button"
+            add_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, item_xpath)))
+            add_button.click()
+
+    def add_all_items_to_cart(self):
+        """
+        加入所有商品到購物車
+        """
+        add_buttons = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//button[contains(text(), 'Add to cart')]"))
+        )
+
+        for button in add_buttons:
+            button.click()
+        print(f"✅ 成功將 {len(add_buttons)} 件商品加入購物車！")
